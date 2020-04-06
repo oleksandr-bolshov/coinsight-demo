560342583383b7c6e0fb4ed7b26d6dc4bffcf8d4
oleksandr-bolshov <o.bolshov@student.csn.khai.edu>
2020-04-06 19:33:17 +0300

    feat(client): Initial commit

---
+++ server/app/Http/Requests/DefaultRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+final class DefaultRequest extends ApiRequest
+{
+    use AuthContextTrait;
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
